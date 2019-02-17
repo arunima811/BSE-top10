240465954fe3524f9e510f01592bdbc630f26477
Arunima Shukla <arunima.shukla81196@gmail.com>
2019-02-17 15:30:42 +0530

    heroku build update

--- app.py
+++ app.py
@@ -37,7 +37,11 @@ class SearchService:
         
 if __name__ == '__main__':
     conf = {
-        '/': {
+        'global': {
+            'server.socket_host': '0.0.0.0',
+            'server.socket_port': int(os.environ.get('PORT', 5000)),
+    	},
+	'/': {
             'tools.sessions.on': True,
             'tools.staticdir.root': os.path.abspath(os.getcwd())
         },
